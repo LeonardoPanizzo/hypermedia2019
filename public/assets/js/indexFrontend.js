4af66b873cfba1911f11d48d0c7130410d44d872
nicholasmagatti <nicholas.magatti@hotmail.it>
2019-08-11 18:07:11 +0200

    fix details

---
+++ public/assets/js/indexFrontend.js
@@ -0,0 +1,18 @@
+//DEPRECATED
+/*
+//load the toolbar
+$(function(){
+    $("#toolbar_html").load("../pages/toolbar.html");
+});*/
+
+// TODO: uncomment this part when it is on the server
+//this part should be commented while using the site on localhost
+/*
+$(document).ready(function(){
+  let ad=window.location.href;
+  ad=ad.substring(4,5);
+  if(ad!='s'){
+    window.location.replace(DOMAIN_ADDRESS);
+  }
+})
+*/
