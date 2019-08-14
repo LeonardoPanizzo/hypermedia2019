9ac4c45accf46ca6263e89b499efc4da2100a3cb
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-08-14 09:28:12 +0200

    Create connectionDB.js

---
+++ other/js/connection/connectionDB.js
@@ -0,0 +1,12 @@
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : 'http://ec2-54-247-170-5.eu-west-1.compute.amazonaws.com',
+    user : 'olheaeorfhaakh',
+    password : 'c1f137921f8051c460f249af820cbdef03b3daf22be26477e60f1917b3464f1a',
+    database : 'd4bg770g99j223',
+    port:5432
+  }
+});
+
+module.exports=knex;
