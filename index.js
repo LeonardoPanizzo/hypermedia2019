fe4fd0b8a9ae19ebec9171b7847c18a0a98cd8da
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-08-14 10:01:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var bodyParser = require('body-parser');
 var cookieParser=require('cookie-parser');
 
 var db = require('./other/js/connection/connectionDB.js');
+var userRoot= require('./other/js/connection/rootUser.js')
 
 var app = express();
 app.use(cookieParser());
 
 app.use(express.static(__dirname+"/public"));
 
+app.use('/querybook', bookrouter);
 
 var PORT=process.env.PORT || 3000;
 app.listen(PORT, function () {
