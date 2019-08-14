73d2e1138c4d2fd77459462826d15fcc972cafdf
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-08-14 19:37:14 +0200

    Update users.js

--- other/js/query/users.js
+++ other/js/query/users.js
@@ -5,17 +5,3 @@ module.exports={
     return db.select('iduser').from('users').where('mail',name).andWhere('password',pass);
   },
 }
-
-/*const logIn=(req,res)=>{
-  db.select('iduser').from('users').where({
-    mail:req.body.name,
-    password:req.body.pwd
-  }).then(function(data){
-    res.send(data)
-  })
-}
-
-
-
-module.exports={logIn}
-*/
