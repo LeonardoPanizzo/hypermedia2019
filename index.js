9493cf9b0cbd6267e48c6b9182c525fd2e5dd925
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-08-15 16:42:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var bodyParser = require('body-parser');
 var cookieParser=require('cookie-parser');
 
 var db = require('./other/js/connection/connectionDB.js');
-var userRoot= require('./other/js/connection/rootUser.js')
+var userLog= require('./other/js/connection/rootLog.js');
+var userSign= require('./other/js/connection/rootSign.js');
 
 var app = express();
 app.use(cookieParser());
@@ -12,7 +13,8 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(express.static(__dirname+"/public"));
 
-app.use('/user', userRoot);
+app.use('/user', userLog);
+//app.use('/user', userLog);
 
 var PORT=process.env.PORT || 3000;
 app.listen(PORT, function () {
