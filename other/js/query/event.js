4129733b61b1e14a8953c05499c31f218145f437
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-08-21 12:37:00 +0200

    Update event.js

--- other/js/query/event.js
+++ other/js/query/event.js
@@ -1,9 +1,23 @@
 var db = require('../connection/connectionDB');
 
 const getall=(req,res)=>{
-  db.select('idevent','title','description','place','dateAndTime','type').from('artisticEvent').orderBy('dateAndTime').then(function(data){
+  db.select('idevent','title','description','place','dateAndTime','type')
+  .from('artisticEvent').orderBy('dateAndTime').then(function(data){
     res.json(data);
   })
 }
 
-module.exports={getall}
+const getType=(req,res)=>{
+  db.select('type').from('artisticEvent').orderBy('type').then(function(data){
+    res.json(data);
+  })
+}
+
+const getToday=(req,res)=>{
+  db.select('idevent','title','description','place','dateAndTime','type')
+  .from('artisticEvent').where("").orderBy('dateAndTime').then(function(data){
+    res.json(data);
+  })
+}
+
+module.exports={getall,getToday,getType}
