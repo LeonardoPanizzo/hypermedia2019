d6a60130a771ad2117f98e1077cb44de7330ce88
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-08-21 12:36:55 +0200

    Update rootEvent.js

--- other/js/connection/rootEvent.js
+++ other/js/connection/rootEvent.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var eventQuery=require('../query/event.js')
 
 router.get('/all', eventQuery.getall);
+router.get('/today', eventQuery.getToday);
+router.get('/type', eventQuery.getType);
 
 module.exports=router;
