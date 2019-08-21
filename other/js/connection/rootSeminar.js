71b900112d5673a7e783f7d2524e260a5924b5fe
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-08-21 12:37:02 +0200

    Update rootSeminar.js

--- other/js/connection/rootSeminar.js
+++ other/js/connection/rootSeminar.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var seminarQuery=require('../query/seminar.js')
 
 router.get('/all',seminarQuery.getall);
+router.get('/today',seminarQuery.getToday);
 
 module.exports=router;
