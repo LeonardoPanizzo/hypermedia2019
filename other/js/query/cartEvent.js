69b63b35f86277d8b042d5dab5c274063c27a2bf
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-08-22 17:08:45 +0200

    Update cartEvent.js

--- other/js/query/cartEvent.js
+++ other/js/query/cartEvent.js
@@ -1,8 +1,8 @@
 var db = require('../connection/connectionDB');
 
 const all=(req,res)=>{
-  db.select('artisticEvent.idevent','title','description','place','dateAndTime','type').from('artisticEvent').join('cartArtisticEvent',{'artisticEvent.idevent','cartArtisticEvent.idevent'}).where('iduser',req.cookies.iduser).then(function(data){
-    res.send(data)
+  db.select('artisticEvent.idevent','title','description','place','dateAndTime','type').from('artisticEvent').join('cartArtisticEvent',{'artisticEvent.idevent':'cartArtisticEvent.idevent'}).where('iduser',req.cookies.iduser).then(function(data){
+    res.send(data);
   })
 }
 
