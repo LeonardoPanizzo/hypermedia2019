a185c10db04a002bff5d9004446dbacfc8624d88
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-08-22 15:14:42 +0200

    Update cartSeminar.js

--- other/js/query/cartSeminar.js
+++ other/js/query/cartSeminar.js
@@ -1,26 +1,9 @@
 var db = require('../connection/connectionDB');
 
-const getall=(req,res)=>{
-  db.select('idseminar','place','title','description','dateAndTime').from('seminar').orderBy('dateAndTime').then(function(data){
-    res.json(data);
-  })
-}
-
-const getToday=(req,res)=>{
-  var d= new Date();
-  var m=d.getMonth()+1;
-  var da=d.getFullYear()+'-'+m+'-'+d.getDate()+' 00:00:00';
-  var dd=d.getFullYear()+'-'+m+'-'+d.getDate()+' 23:59:59';
-  db.select('idseminar','place','title','description','dateAndTime')
-  .from('seminar').where('dateAndTime','>=', da).andWhere('dateAndTime','<=',dd).orderBy('dateAndTime').then(function(data){
-    res.json(data);
-  })
-}
-
-const getbyID=(req,res)=>{
+const bb=(req,res)=>{
   db.select('idseminar','place','title','description','dateAndTime').from('seminar').where('idseminar',req.params.id).then(function(data){
     res.send(data)
   })
 }
 
-module.exports={getall,getToday,getbyID}
+module.exports={bb}
