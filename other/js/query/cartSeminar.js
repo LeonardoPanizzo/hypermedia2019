7756e24074c9b53ccc98a8316584f3278cda102d
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-08-23 20:33:14 +0200

    Update cartSeminar.js

--- other/js/query/cartSeminar.js
+++ other/js/query/cartSeminar.js
@@ -2,7 +2,7 @@ var db = require('../connection/connectionDB');
 
 const all=(req,res)=>{
   db.select('seminar.idseminar','place','title','description','dateAndTime').from('cartSeminar').join('seminar',{'seminar.idseminar':'cartSeminar.idseminar'}).where('iduser',req.cookies.iduser).then(function(data){
-    res.send(data)
+    res.json(data)
   })
 }
 
