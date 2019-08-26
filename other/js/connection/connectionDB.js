25ff0ba0a9ee5d409311197f267406153bb18038
nicholasmagatti <nicholas.magatti@hotmail.it>
2019-08-26 14:04:09 +0200

    for test in remote

--- other/js/connection/connectionDB.js
+++ other/js/connection/connectionDB.js
@@ -1,15 +1,15 @@
 var knex = require('knex')({
-
+  /*
   client: 'pg',
   connection: {
     host : '127.0.0.1',
     user : 'postgres',
-    password : '123456789',
-    //password : '290396',
+    //password : '123456789',
+    password : '290396',
     database : 'aaaa',
     port:5432
   }
-  /*
+  */
   client: 'pg',
   connection: {
     host : 'ec2-54-247-170-5.eu-west-1.compute.amazonaws.com',
@@ -17,7 +17,7 @@ var knex = require('knex')({
     password : 'c1f137921f8051c460f249af820cbdef03b3daf22be26477e60f1917b3464f1a',
     database : 'd4bg770g99j223',
     port:5432
-  }*/
+  }
 });
 
 module.exports=knex;
