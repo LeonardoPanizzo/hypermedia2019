3344b6c25dea07512574198464ad96ce1cf0a514
nicholasmagatti <nicholas.magatti@hotmail.it>
2019-08-29 19:07:23 +0200

    complete managing of reserv button in artistic event

--- public/assets/js/artisticEventFrontend.js
+++ public/assets/js/artisticEventFrontend.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
   var id =  getIdFromUrlBeforeQuestionMark(DOMAIN_ADDRESS + "/pages/artisticEvent.html");
-
-  $.get(DOMAIN_ADDRESS + "/reservationArtisticEvent/" + id, function(answer){
-    alert("answer.message: " + answer.message);
-    var alreadyGotReservation = Boolean(answer.message);
-    alert("alreadyGotReservation: " + alreadyGotReservation);
-
-    $("#buttonGetReservation").click(function(){
-      if(document.cookie){
+  var alreadyGotReservation = false; //if the user is not logged, show his the button to get reservation
+  if(document.cookie){
+    $.get(DOMAIN_ADDRESS + "/reservationArtisticEvent/" + id, function(answer){
+      alreadyGotReservation = answer.message;
+      //alert("alreadyGotReservation: " + alreadyGotReservation);
+      if(alreadyGotReservation){
+        $("#buttonGetReservation").hide();
+        $('#infoReservationForEventPage').append("<div class='reserved_event_label'>Ticket reserved <i class='material-icons'>check_circle_outline</i></div>");
+      }
+      $("#buttonGetReservation").click(function(){
         if(alreadyGotReservation){
-          //you already have a reservation for this event
-          alert("TODO!!");
+          alert("TODO!");
         }
-        else{
+        else{//add reservation for this event
           $.ajax({
             url: DOMAIN_ADDRESS + '/reservationArtisticEvent',
             type: 'POST',
@@ -21,17 +22,45 @@ $(document).ready(function(){
             },
             dataType: 'json',
             success:(data)=>{
-
+              window.location.replace(window.location.href);
             }
-          })
+          });
         }
-      }
-      else{
+      });
+    });
+  }
+  else{ //if not logged
+    $("#buttonGetReservation").click(function(){
         alert("You should log in first!");
         window.location.assign( DOMAIN_ADDRESS + "/pages/login.html");
-      }
     });
-  });
+  }
+/*
+  $("#buttonGetReservation").click(function(){
+    if(document.cookie){
+      if(alreadyGotReservation){
+        //you already have a reservation for this event
+        alert("TODO!!");
+      }
+      else{
+        $.ajax({
+          url: DOMAIN_ADDRESS + '/reservationArtisticEvent',
+          type: 'POST',
+          data:{
+            'id': id
+          },
+          dataType: 'json',
+          success:(data)=>{
+
+          }
+        })
+      }
+    }
+    else{
+      alert("You should log in first!");
+      window.location.assign( DOMAIN_ADDRESS + "/pages/login.html");
+    }
+  });*/
 
   $.get(DOMAIN_ADDRESS + "/artisticEvent/" + id, function(result){
     var artisticEvent = result[0];
