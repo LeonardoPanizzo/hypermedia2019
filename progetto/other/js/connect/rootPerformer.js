cd14a769682bbcc6d5a2300c2a9f095a8057efb0
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-09-03 23:48:17 +0200

    update

--- progetto/other/js/connect/rootPerformer.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var performerQuery=require('../query/performer.js')
-
-router.get('/',performerQuery.getall);
-router.get('/:id',performerQuery.getbyID);
-router.get('/artisticEvent/:id',performerQuery.getbyEvent);
-
-module.exports=router;
