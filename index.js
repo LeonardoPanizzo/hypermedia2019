3badb29d409e19229960e5688b34d0f161bc8696
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-09-03 21:23:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var reservationEventR=require('./other/js/connection/rootReservationEvent.js');
 
 var app = express();
 app.use(cookieParser());
-aap.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }))
