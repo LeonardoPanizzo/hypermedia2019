3ff66acfe2d1493de80e5fd4a05e8c07f88f595b
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-09-03 20:59:30 +0200

    up

--- public/assets/js/loginFrontend.js
+++ public/assets/js/loginFrontend.js
@@ -8,18 +8,14 @@ $(document).ready(function(){
 $("#buttonLogin").click(function(){
   let email=$("#email").val();
   email=email.toLowerCase();
-  let pass=$("#pass").val();
+  let pass=$("#password").val();
   if(email.length===0 || pass.length===0){
     alert('Insert all the required data');
   }else{
     $.ajax({
       url: DOMAIN_ADDRESS+'/user/login',
       type: 'POST',
-      data:{
-        'mail':email,
-        'pass':pass
-      },
-      dataType: 'json',
+      data:$("#loginform").serialize(),
       success:(data)=>{
         if(data.loggedin){
           alert('logged in');
